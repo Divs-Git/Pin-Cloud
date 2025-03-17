34d98740ad98919e0314c2153a2d71b9a6242d94
Divs-Git <divyansh.sri258@gmail.com>
2025-03-17 20:15:48 +0530

    added hover overlay and buttons

--- client/src/components/galleryItem/GalleryItem.jsx
+++ client/src/components/galleryItem/GalleryItem.jsx
@@ -1,4 +1,5 @@
 import './galleryItem.css';
+import { Link } from 'react-router';
 
 const GalleryItem = ({ item }) => {
   return (
@@ -7,6 +8,16 @@ const GalleryItem = ({ item }) => {
       style={{ gridRowEnd: `span ${Math.ceil(item.height / 100)}` }}
     >
       <img src={item.media} alt='image' />
+      <Link to={`/pin/${item.id}`} className='overlay' />
+      <button className='saveBtn'>Save</button>
+      <div className='overlayIcons'>
+        <button>
+          <img src='/general/share.svg' />
+        </button>
+        <button>
+          <img src='/general/more.svg' />
+        </button>
+      </div>
     </div>
   );
 };
