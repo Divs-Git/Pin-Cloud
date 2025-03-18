884946b7add52277f3878817ac223295e57cbae6
Divs-Git <divyansh.sri258@gmail.com>
2025-03-18 20:58:08 +0530

    added high level routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,21 @@
 import express from 'express';
+import userRouter from './routes/user.route.js';
+import pinRouter from './routes/pin.route.js';
+import commentRouter from './routes/comment.route.js';
+import boardRouter from './routes/board.route.js';
 
 const app = express();
 
+// app.use('/api/test', (req, res) => {
+//   return res.json('Hello from backend');
+// });
+
 const port = process.env.PORT;
+
+app.use('/api/users', userRouter);
+app.use('/api/pins', userRouter);
+app.use('/api/comments', userRouter);
+app.use('/api/boards', userRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
