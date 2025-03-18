a7068e8a8260ca17773ca887f3290ff5e87aff71
Divs-Git <divyansh.sri258@gmail.com>
2025-03-18 17:02:32 +0530

    added post interactions UI

--- client/src/components/comments/Comments.jsx
+++ client/src/components/comments/Comments.jsx
@@ -1,5 +1,90 @@
+import { useState } from 'react';
+import Image from '../image/Image';
+import './comments.css';
+import EmojiPicker from 'emoji-picker-react';
+
 const Comments = () => {
-  return <div>Comments</div>;
+  const [open, setOpen] = useState(false);
+  return (
+    <div className='comments'>
+      <div className='commentList'>
+        <span className='commentCount'>6 comments</span>
+
+        {/**COMMENT */}
+        <div className='comment'>
+          <Image path={'/general/noAvatar.png'} alt={''} />
+
+          <div className='commentContent'>
+            <span className='commentUsername'>Divyansh Srivastava</span>
+            <p className='commentText'>This is a very beautiful post image.</p>
+            <span className='commentTime'>2h</span>
+          </div>
+        </div>
+        {/**COMMENT */}
+        <div className='comment'>
+          <Image path={'/general/noAvatar.png'} alt={''} />
+
+          <div className='commentContent'>
+            <span className='commentUsername'>Divyansh Srivastava</span>
+            <p className='commentText'>This is a very beautiful post image.</p>
+            <span className='commentTime'>2h</span>
+          </div>
+        </div>
+        {/**COMMENT */}
+        <div className='comment'>
+          <Image path={'/general/noAvatar.png'} alt={''} />
+
+          <div className='commentContent'>
+            <span className='commentUsername'>Divyansh Srivastava</span>
+            <p className='commentText'>This is a very beautiful post image.</p>
+            <span className='commentTime'>2h</span>
+          </div>
+        </div>
+        {/**COMMENT */}
+        <div className='comment'>
+          <Image path={'/general/noAvatar.png'} alt={''} />
+
+          <div className='commentContent'>
+            <span className='commentUsername'>Divyansh Srivastava</span>
+            <p className='commentText'>This is a very beautiful post image.</p>
+            <span className='commentTime'>2h</span>
+          </div>
+        </div>
+        {/**COMMENT */}
+        <div className='comment'>
+          <Image path={'/general/noAvatar.png'} alt={''} />
+
+          <div className='commentContent'>
+            <span className='commentUsername'>Divyansh Srivastava</span>
+            <p className='commentText'>This is a very beautiful post image.</p>
+            <span className='commentTime'>2h</span>
+          </div>
+        </div>
+        {/**COMMENT */}
+        <div className='comment'>
+          <Image path={'/general/noAvatar.png'} alt={''} />
+
+          <div className='commentContent'>
+            <span className='commentUsername'>Divyansh Srivastava</span>
+            <p className='commentText'>This is a very beautiful post image.</p>
+            <span className='commentTime'>2h</span>
+          </div>
+        </div>
+      </div>
+
+      <form className='commentForm'>
+        <input type='text' placeholder='Add a comment' />
+        <div className='emoji'>
+          <div onClick={() => setOpen((prev) => !prev)}>😊</div>
+          {open && (
+            <div className='emojiPicker'>
+              <EmojiPicker />
+            </div>
+          )}
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Comments;
--- client/src/components/postInteractions/PostInteractions.jsx
+++ client/src/components/postInteractions/PostInteractions.jsx
@@ -1,5 +1,18 @@
+import Image from '../image/Image';
+import './postInteractions.css';
+
 const PostInteractions = () => {
-  return <div>PostInteractions</div>;
+  return (
+    <div className='postInteractions'>
+      <div className='interactionIcons'>
+        <Image path={'/general/react.svg'} alt={''} />
+        181
+        <Image path={'/general/share.svg'} alt={''} />
+        <Image path={'/general/more.svg'} alt={''} />
+      </div>
+      <button>Save</button>
+    </div>
+  );
 };
 
 export default PostInteractions;
