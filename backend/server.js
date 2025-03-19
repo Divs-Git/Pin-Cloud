052f0cdacbe2e357e7a3283922ed6a5eabc7d38e
Divs-Git <divyansh.sri258@gmail.com>
2025-03-19 18:29:04 +0530

    fiex cors issue

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,15 @@ import pinRouter from './routes/pin.route.js';
 import commentRouter from './routes/comment.route.js';
 import boardRouter from './routes/board.route.js';
 import connectDB from './utils/connectDB.js';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
 
 // app.use('/api/test', (req, res) => {
 //   return res.json('Hello from backend');
@@ -15,9 +21,9 @@ app.use(express.json());
 const port = process.env.PORT;
 
 app.use('/api/users', userRouter);
-app.use('/api/pins', userRouter);
-app.use('/api/comments', userRouter);
-app.use('/api/boards', userRouter);
+app.use('/api/pins', pinRouter);
+app.use('/api/comments', commentRouter);
+app.use('/api/boards', boardRouter);
 
 app.listen(port, () => {
   connectDB();
