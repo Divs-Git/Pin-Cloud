96db8569494d3fa06beaa7bb861320ca1f8d85bb
Divs-Git <divyansh.sri258@gmail.com>
2025-03-21 22:37:14 +0530

    added edit design logic

---
+++ client/src/components/editor/Editor.jsx
@@ -0,0 +1,7 @@
+import './editor.css';
+
+const Editor = () => {
+  return <div>Editor</div>;
+};
+
+export default Editor;
--- client/src/pages/create/Create.jsx
+++ client/src/pages/create/Create.jsx
@@ -1,68 +1,112 @@
 import Image from '../../components/image/Image';
 import './create.css';
+import useAuthStore from './../../store/authStore';
+import { useNavigate } from 'react-router';
+import { Fragment, useEffect, useState } from 'react';
+import Editor from '../../components/editor/Editor';
 
 const Create = () => {
+  const { currentUser } = useAuthStore();
+  const navigate = useNavigate();
+
+  const [file, setFile] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+
+  useEffect(() => {
+    if (!currentUser) {
+      navigate('/auth');
+    }
+  }, [navigate, currentUser]);
+
+  const previewImageURL = file ? URL.createObjectURL(file) : null;
+
   return (
     <div className='createPage'>
       <div className='createTop'>
-        <h1>Create Pin</h1>
-        <button>Publish</button>
+        <h1>{isEditing ? 'Edit' : 'Create'} Pin</h1>
+        <button>{isEditing ? 'Done' : 'Publish'}</button>
       </div>
 
-      <div className='createBottom'>
-        <div className='upload'>
-          <div className='uploadTitle'>
-            <Image path={'/general/upload.svg'} alt={''} />
-            <span>Choose a file</span>
-          </div>
-          <div className='uploadInfo'>
-            We recommend using high quality .jpg files less than 200 MB.
-          </div>
+      {isEditing ? (
+        <Editor />
+      ) : (
+        <div className='createBottom'>
+          {previewImageURL ? (
+            <div className='preview'>
+              <img src={previewImageURL} />
+              <div className='editIcon' onClick={() => setIsEditing(true)}>
+                <Image path={'/general/edit.svg'} alt={''} />
+              </div>
+            </div>
+          ) : (
+            <Fragment>
+              <label htmlFor='file' className='upload'>
+                <div className='uploadTitle'>
+                  <Image path={'/general/upload.svg'} alt={''} />
+                  <span>Choose a file</span>
+                </div>
+                <div className='uploadInfo'>
+                  We recommend using high quality .jpg files less than 200 MB.
+                </div>
+              </label>
+              <input
+                type='file'
+                id='file'
+                hidden
+                onChange={(e) => setFile(e.target.files[0])}
+              />
+            </Fragment>
+          )}
+          <form className='createForm'>
+            <div className='createFormItem'>
+              <label htmlFor='title'>Title</label>
+              <input
+                type='text'
+                placeholder='Add a title'
+                name='title'
+                id='title'
+              />
+            </div>
+
+            <div className='createFormItem'>
+              <label htmlFor='description'>Description</label>
+              <textarea
+                rows={6}
+                type='text'
+                placeholder='Add a description'
+                name='description'
+                id='description'
+              />
+            </div>
+
+            <div className='createFormItem'>
+              <label htmlFor='link'>Link</label>
+              <input
+                type='text'
+                placeholder='Add a link'
+                name='link'
+                id='link'
+              />
+            </div>
+
+            <div className='createFormItem'>
+              <label htmlFor='board'>Board</label>
+              <select name='board' id='board'>
+                <option>Choose a board</option>
+                <option value={'1'}>Board 1</option>
+                <option value={'2'}>Board 2</option>
+                <option value={'3'}>Board 3</option>
+              </select>
+            </div>
+
+            <div className='createFormItem'>
+              <label htmlFor='tags'>Tagged topics</label>
+              <input type='text' placeholder='Add tags' name='tags' id='tags' />
+              <small>Don&apos;t worry, people won&apos;t see your tags</small>
+            </div>
+          </form>
         </div>
-        <form className='createForm'>
-          <div className='createFormItem'>
-            <label htmlFor='title'>Title</label>
-            <input
-              type='text'
-              placeholder='Add a title'
-              name='title'
-              id='title'
-            />
-          </div>
-
-          <div className='createFormItem'>
-            <label htmlFor='description'>Description</label>
-            <textarea
-              rows={6}
-              type='text'
-              placeholder='Add a description'
-              name='description'
-              id='description'
-            />
-          </div>
-
-          <div className='createFormItem'>
-            <label htmlFor='link'>Link</label>
-            <input type='text' placeholder='Add a link' name='link' id='link' />
-          </div>
-
-          <div className='createFormItem'>
-            <label htmlFor='board'>Board</label>
-            <select name='board' id='board'>
-              <option>Choose a board</option>
-              <option value={'1'}>Board 1</option>
-              <option value={'2'}>Board 2</option>
-              <option value={'3'}>Board 3</option>
-            </select>
-          </div>
-
-          <div className='createFormItem'>
-            <label htmlFor='tags'>Tagged topics</label>
-            <input type='text' placeholder='Add tags' name='tags' id='tags' />
-            <small>Don&apos;t worry, people won&apos;t see your tags</small>
-          </div>
-        </form>
-      </div>
+      )}
     </div>
   );
 };
