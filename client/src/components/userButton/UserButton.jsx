5dab52cd504a350e2cb828b5618b8212d92262d3
Divs-Git <divyansh.sri258@gmail.com>
2025-04-10 21:23:07 +0530

    updated css

--- client/src/components/userButton/UserButton.jsx
+++ client/src/components/userButton/UserButton.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './userButton.css';
+import './userButtonStyle.css';
 import { Link, useNavigate } from 'react-router';
 import useAuthStore from '../../stores/authStore';
 import axiosInstance from '../../api';
